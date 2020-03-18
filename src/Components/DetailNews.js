bee4d4a2ebad767143e4772cf1c8fb3e1ad4a20c
unknown <demophp0205chat>
2020-03-18 14:14:47 +0700

    css width

--- src/Components/DetailNews.js
+++ src/Components/DetailNews.js
@@ -9,77 +9,60 @@ class DetailNews extends Component {
 
     render() {
 
-        // var current_new = ''
-        // data.forEach((value,key) => {
-        //     if (value.id == id){
-        //         current_new= value;
-        //         return
-        //     }
-        // });
                 
         return (
         <div className="mb-5">
-                <header className="masthead_1">
-                    <div className="container ">
-                    <div className="row">
-                        {/* <div class="col-lg-7 my-auto">
-                    <div class="header-content mx-auto">
-                    <h1 class="mb-5">New Age is an app landing page that will help you beautifully showcase your new mobile app, or anything else!</h1>
-                    <a href="#download" class="btn btn-outline btn-xl js-scroll-trigger">Start Now for Free!</a>
-                    </div>
-                </div> */}
-                        <div className="col-12 justify-content-center d-flex mt-5">
-                        <h1 className="mt-5 text-info font-weight-normal">DETAIL NEWS PAGE</h1>
-                        </div>
-                    </div>
-                    </div>
-                </header>
+            <header className="masthead_1">
                 <div className="container ">
-                    <div id="news_id_master" className="news ">
-                        {                            
-                            data.map( (value,key) => {
-                                        if (value.id === parseInt(this.props.match.params.id)  ){
-                                            return (
-                                                <div className="card-group m-5 col-10" key = {key}>        
-                                                    <div className="card border-0">
-                                                    <img className="card-img-top p-5" src={value.img} alt="" />
-                                                    <div className="card-body">
-                                                        <h3 className="card-title text-info">{value.title}</h3>
-                                                        <p className="card-text">{value.content}</p>
-                                                        <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                                                    </div>
-                                                    </div>
-                                                </div>    
-                                            )
-                                        }
-                                        return true
-                                    }
-                            )
-                        }
-
-                        <div className="card-group">
-                                {
-                                    data.map( (value,key) => {
-                                        if( key < 4 ){
-                                            return (
-                                                <RelatedNews key = {key}
-                                                img_ = {value.img}
-                                                title_ = {value.title}
-                                                description_ = {value.description}
-                                                url_ = {"/detail-news/"+to_slug(value.title)+"."+value.id+".html"}
-                                                />
-                                            )
-                                        }
-                                        return true
-                                    })   
-                                }
-                                    
-
-
-                        </div>  
-                    
+                <div className="row">
+                    <div className="col-12 justify-content-center d-flex mt-5">
+                    <h1 className="mt-5 text-info font-weight-normal">DETAIL NEWS PAGE</h1>
                     </div>
                 </div>
+                </div>
+            </header>
+
+            <div className="container col-12 ">
+                <div id="news_id_master text-center row col-12" className="news ">
+                    {                            
+                        data.map( (value,key) => {
+                            if (value.id === parseInt(this.props.match.params.id)  ){
+                                return (
+                                    <div className="card-group col-12" key = {key}>        
+                                        <div className="card border-0 pl-5 pr-5 ml-5 mr-5">
+                                        <img className="card-img-top p-5" src={value.img} alt="" />
+                                        <div className="card-body pl-5 pr-5">
+                                            <h3 className="card-title text-info">{value.title}</h3>
+                                            <p className="card-text">{value.content}</p>
+                                            <p className="card-text">{value.content}</p>
+                                            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                                        </div>
+                                        </div>
+                                    </div>    
+                                )
+                            }
+                            return true
+                        })
+                    }
+
+                    <div className="card-group">
+                        {
+                            data.map( (value,key) => {
+                                if( key < 4 ){
+                                    return (
+                                        <RelatedNews key = {key}
+                                        img_ = {value.img}
+                                        title_ = {value.title}
+                                        description_ = {value.description}
+                                        url_ = {"/detail-news/"+to_slug(value.title)+"."+value.id+".html"}
+                                        />
+                                )}
+                                return true
+                            })   
+                        }                                
+                    </div>                  
+                </div>
+            </div>
         </div>
         );
     }
